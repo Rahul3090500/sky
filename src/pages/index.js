c3718ae3187bd65a0fb81b44bea14c3c5d6319c5
Rahul <109056697+Rahul3090500@users.noreply.github.com>
2023-07-15 02:48:41 +0530

    changes

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import '../styles/Home.module.scss'
+import { App as Canvas } from '../components/Canvas'
+import Overlay from '../components/OverLay'
+
+export default function Home() {
+  return (
+
+  <>
+    <Canvas />
+    <Overlay />
+  </>
+)
+
+}
